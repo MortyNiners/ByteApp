b4545166a5c6c90041aed9d4f3ce81f8853ca178
Marius Sirbu <mariuss.sirbu@yandex.com>
2023-10-30 15:33:18 +0200

    created routes

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      'gray-dark': '#1E1E20',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
