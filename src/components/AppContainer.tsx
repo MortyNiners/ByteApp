ed7ad04e02543bd4722e1ac60ff48caab99bf489
MortyNiners <mariuss.sirbu@yandex.com>
2023-11-09 14:47:28 +0200

    user section ui ready

--- src/components/AppContainer.tsx
+++ src/components/AppContainer.tsx
@@ -4,10 +4,10 @@ import { User } from './User.tsx';
 export const AppContainer = () => {
   return (
     <>
-      <HeaderContainer />
-      <section className=" min-w-[320px]">
+      <section className="fixed min-w-full ">
+        <HeaderContainer />
         <div className="grid grid-cols-12">
-          <div className="col-span-3">
+          <div className="col-span-3 ">
             <User />
           </div>
           <div className="col-span-3"></div>
